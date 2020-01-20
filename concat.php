654a4c63636339ec08934d884e798d8ee19492ab
Amoo Pedram <Pedroxam@gmail.com>
2020-01-20 05:19:45 +0300

    Add files via upload

---
+++ concat.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* By Pedram
+* Telegram: @Pedroxam
+* Email: pedroxam@gmail.com
+*/
+
+$root = dirname(__FILE__);
+
+$files = glob('output/*');
+
+if(!count($files)) exit;
+
+foreach($files as $file){
+	file_put_contents('./list.txt', "file '$file'" . "\n", FILE_APPEND);
+}
+
+$output = $root . '/output/' . 'concated_' . time();
+
+$command = "ffmpeg -f concat -safe 0 -i ./list.txt -c copy $output.mp4";
+
+$log = './concat_log.txt';
+
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+{
+    pclose(popen("start /B " . $command . " 1> $log 2>&1", "r")); // windows
+}
+else
+{
+    shell_exec($command . " 1> $log 2>&1 >/dev/null &"); //linux
+}
